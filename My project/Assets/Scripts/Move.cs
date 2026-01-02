18572593fb3af68254ab970789dc149a2259537a
Deathstriker387 <shoaibfarooq122@gmail.com>
2026-01-02 21:01:27 +0500

    Movement and jump script

--- My project/Assets/Scripts/Move.cs
+++ My project/Assets/Scripts/Move.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5.0f;
+    public float jumpForce = 10.0f;
+
+    private Rigidbody2D rb;
+    private float horizontalInput;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
